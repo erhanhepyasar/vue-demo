bb918196797e937d8691a57366f6d2c0d6811ed2
Erhan Hepyasar <erhan.hepyasar@gmail.com>
2020-08-17 10:29:36 +0300

    files added

--- Exercises/Exercise 4/app.js
+++ Exercises/Exercise 4/app.js
@@ -1,11 +1,23 @@
-new Vue({
+var app = new Vue({
   el: '#exercise',
   data: {
     toggleClass: false,
     selectedClass: '',
     classA: 'a',
     classB: 'b',
-    selectedClass2: ''
+    selectedClass2: '',
+    selectedClass3: '',
+    activateClass3: 'true',
+    selectedStyle1: 'red',
+    selectedColor: 'purple',
+    barLen: 0
+  },
+  computed: {
+    myStyle: function() {
+      return {
+          backgroundColor: this.selectedColor,
+      }
+  }
   },
   methods: {
     startEffect: function() {
@@ -15,6 +27,13 @@ new Vue({
         vm.toggleClass = !vm.toggleClass;
         vm.selectedClass = vm.toggleClass ? 'highlight' : 'shrink';
         console.log('interval - ' + vm.selectedClass)
+      }, 1000)
+    },
+    startProgress: function() {
+      var vm = this;
+      console.log('start progress')
+      setInterval(function(){
+        vm.barLen += 25
       }, 1000)
     }
   }
