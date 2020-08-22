08e7a5c76a846326b61daf6fd1884ae10365c9f7
Erhan Hepyasar <erhan.hepyasar@softtech.com.tr>
2020-08-22 06:37:00 +0300

    added

---
+++ cli/vue-props/src/components/User.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my name</button>
+        <p>Name is {{ name }}</p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail 
+                    :myName="name" 
+                    @nameWasReset="name = $event"
+                    :resetFn="resetName"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import userDetail from './UserDetails.vue'
+    import userEdit from './UserEdit.vue'
+
+    export default {
+        data: function() {
+            return {
+                name: 'Erhan'
+            }
+        },
+        methods: {
+            changeName() {           
+                this.name = 'Hepyasar'        
+            },
+            resetName() {
+                this.name = 'Initial name'
+            }
+        },
+        components: {
+            'app-user-detail': userDetail,
+            'app-user-edit': userEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ cli/vue-props/src/components/UserDetails.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User name: {{ myName }}</p>
+        <p>Reverse name: {{ reverseName() }}</p>
+        <button @click="resetName">Reset Name</button>
+        <button @click="resetFn()">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            myName: { // parent -> child
+                type: String,
+                required: true
+            },
+            resetFn: Function // 2.callback (child -> parent)
+        
+        },
+        methods: {
+            reverseName() {
+                return this.myName.split("").reverse().join("")
+            },
+            resetName() {
+                this.myName = 'Max'
+                this.$emit('nameWasReset', this.myName) // 1.custom event (child -> parent)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
