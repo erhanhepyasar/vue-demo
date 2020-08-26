d98bec8c96f4b90dacbe5ae0ebd8b8ac06378e69
Erhan Hepyaşar <200189@gm.isbank.com.tr>
2020-08-26 14:35:04 +0300

    add

--- 5-directives/exercise/src/App.vue
+++ 5-directives/exercise/src/App.vue
@@ -5,15 +5,48 @@
                 <h1>Directives Exercise</h1>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-                <p v-myon:click="">Demo</p>
+                <button id="p1" v-customOn:click="clicked" class="btn btn-primary">Demo</button>
+                <hr>
+                <div class="box-green"
+                     v-customOn:mouseenter="mouseEnter"
+                     v-customOn:mouseleave="mouseLeave"></div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        directives: {
+            customOn: {
+                  bind(el, binding) {
+                    // el.onclick = function() {
+                    //     binding.value();
+                    // }
+                    const type = binding.arg;
+                    const fn = binding.value;
+                    el.addEventListener(type, fn)
+                }
+            }
+        },
+        methods: {
+            clicked() {
+                alert('I was clicked!')
+            },
+            mouseEnter() {
+                console.log('Mouse entered!')
+            },
+            mouseLeave() {
+                console.log('Mouse left!')
+            }
+        }
+    }
 </script>
 
 <style>
+    .box-green {
+        width: 100px;
+        height: 100px;
+        background-color: lightgreen;
+    }
 </style>
