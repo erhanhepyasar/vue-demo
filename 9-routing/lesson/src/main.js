129c6a8f0223aaf60e81a4b2ef894f4bc52ecb1a
Erhan Hepyaşar <200189@gm.isbank.com.tr>
2020-08-27 09:47:31 +0300

    routing added

---
+++ 9-routing/lesson/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+import App from './App.vue'
+import { routes } from './routes'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes,
+  // mode: 'hash' // default mode using # in url. No need no write this line of code for default behaviour
+  mode: 'history' // to not to use # in url. 
+  //Need to configure server to return index.html in all cases including 404. See official docs for routing-history
+  //web-pack was already configured so
+});
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
